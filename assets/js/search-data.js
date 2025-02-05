b05fa1603965b9caeff1f872e29788732ccd0e16
mohas95 <mohamed.debbagh@mail.mcgill.ca>
2025-02-05 01:38:19 +0000

    Deploying to gh-pages from @ mohas95/mohas95.github.io@0b6538e05a299adf8919d660b2e3d7e6bb127334 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -128,7 +128,12 @@ ninja.data = [{
           window.location.href = "/lectures/2023/CRF/";
         
       },
-    },{id: "news-hello-world-sparkles-smile",
+    },{id: "datasets-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Datasets",handler: () => {
+              window.location.href = "/datasets/1_project/";
+            },},{id: "news-hello-world-sparkles-smile",
           title: 'Hello World! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
@@ -176,6 +181,51 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "robotfleet-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/1_project/";
+            },},{id: "robotfleet-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/2_project/";
+            },},{id: "robotfleet-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/3_project/";
+            },},{id: "robotfleet-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/4_project/";
+            },},{id: "robotfleet-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/5_project/";
+            },},{id: "robotfleet-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/6_project/";
+            },},{id: "robotfleet-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/7_project/";
+            },},{id: "robotfleet-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/8_project/";
+            },},{id: "robotfleet-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Robotfleet",handler: () => {
+              window.location.href = "/robotfleet/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
